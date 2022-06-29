fca6b5c164e7fecf393a55a33eca62f669a676b1
leejinuuuu <85885245+leejinuuuu@users.noreply.github.com>
2022-06-29 21:58:39 +0900

    2022.06.29

---
+++ baekjoon/1699.py
@@ -0,0 +1,24 @@
+# N = int(input())
+# r = int(N**(1/2))
+# sum = 0
+# count = 0
+# while(True):
+#     sum = sum + (r**2)
+#     r = int((N-sum)**(1/2))
+#     count += 1
+#     if sum == N:
+#         break
+# print(str(count))
+
+N = int(input())
+
+DP = [x for x in range(N+1)]
+for i in range(1, N+1):
+    DP[i] = i
+    for j in range(1, i):
+        if(j**2) > i:
+            break
+        if DP[i] > DP[i-j**2]+1:
+            DP[i] = DP[i-j**2]+1
+
+print(DP[N])
