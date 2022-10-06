417fb61431a724fd5ac66c487d3beda06824f8f6
leejinuuuu <jinw810@gmail.com>
2022-10-06 22:38:02 +0900

    2022.10.06

---
+++ baekjoon/2004.py
@@ -0,0 +1,19 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+def two_count(n):
+    two = 0
+    while n != 0:
+        n = n // 2
+        two += n
+    return two
+
+def five_count(n):
+    five = 0
+    while n != 0:
+        n = n // 5
+        five += n
+    return five
+
+print(min(two_count(n) - two_count(n - m) - two_count(m), five_count(n) - five_count(n - m) - five_count(m)))
